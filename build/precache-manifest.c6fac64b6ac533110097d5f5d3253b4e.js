48744e629117557b2ca8326e4e6bb72059dbf5a3
mariia.pyylampi@gmail.com <mariia.pyylampi@gmail.com>
2020-10-05 14:41:53 +0300

    puhelinluettelo full stack

---
+++ build/precache-manifest.c6fac64b6ac533110097d5f5d3253b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c129fced9cdf84becfd547209e63153b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4a20c1932df94eaa59d",
+    "url": "/static/css/main.15c133c5.chunk.css"
+  },
+  {
+    "revision": "16ee3d411164fc5738fc",
+    "url": "/static/js/2.e657b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e657b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a20c1932df94eaa59d",
+    "url": "/static/js/main.939fdd80.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
