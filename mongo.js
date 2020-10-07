6a1af35e38690de942e35c322b1ce8d929bfe0b0
mariia.pyylampi@gmail.com <mariia.pyylampi@gmail.com>
2020-10-07 14:25:07 +0300

    lint-konfigraatio

--- mongo.js
+++ mongo.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 if (process.argv.length===4) {
-    console.log('add name and number')
-    process.exit(1)
+  console.log('add name and number')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -17,35 +18,35 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.oatak.mongodb.net/phon
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length===3) {
-    console.log('show people')
-    
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  console.log('show people')
+
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length===5) {
-    console.log('adding to phonebook')
-    
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+  console.log('adding to phonebook')
 
-    person.save().then(response => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save().then(response => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
